96a54b02119e5deeea9d00bb0e576fedcd62f4e6
zkan <zkan.cs@gmail.com>
2022-06-07 07:31:27 +0700

    Create staging tutorial model and tests :bear:

---
+++ greenery/models/staging/staging_tutorial/stg_tutorial_superheroes.sql
@@ -0,0 +1,20 @@
+{{
+  config(
+    materialized='table'
+  )
+}}
+
+SELECT
+    id AS superhero_id,
+    name,
+    gender,
+    eye_color,
+    race,
+    hair_color,
+    height,
+    publisher,
+    skin_color,
+    alignment,
+    weight
+FROM {{ source('tutorial', 'superheroes') }}
+
